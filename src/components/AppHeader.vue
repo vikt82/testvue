50d2668956f255ca5f49064ec9225b0d9328198c
Victor V <victorv@Victors-MacBook-Pro.local>
2018-12-01 19:07:27 +0300

    initial

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-toolbar app dark fixed class="primary">
+        <!-- <v-toolbar-side-icon></v-toolbar-side-icon> -->
+        <v-toolbar-title>{{logo}}</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+            <v-btn v-for="item in menuItems" :key="item.id" flat>
+                <i class="material-icons">{{item.icon}}</i>
+                {{item.title}}
+            </v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+
+    data: () => ({
+        "logo": "Logo Type !!!"
+    }),
+
+    computed: {
+        menuItems() {
+            return [
+                {
+                    icon: "android",
+                    title: "Home",
+                    route: "/home"
+                },
+                {
+                    icon: "book",
+                    title: "About Us",
+                    route: "/about-us"
+                },
+                {
+                    icon: "language",
+                    title: "Contact Us",
+                    route: "/contact-us"
+                }
+            ]
+        }
+    }
+
+}
+</script>
+
+<style>
+    .material-icons {
+        margin-right: 1rem;
+    }
+</style>
